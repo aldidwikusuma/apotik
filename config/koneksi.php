cd29978fc0b1c74d6c860241060ba65cd3af3846
aldidwikusuma <aldidwi518@gmail.com>
2022-10-13 11:04:43 +0700

    first commit

--- config/koneksi.php
+++ config/koneksi.php
@@ -4,6 +4,11 @@
     $password = "";
     $dbname = "apm";
 
+    define( 'WP_DEBUG', true );
+    define( 'WP_DEBUG_LOG', true );
+    define( 'WP_DEBUG_DISPLAY', false );
+
+
     $conn = mysqli_connect($servername, $username, $password, $dbname);
     if (!$conn) {
         die("Connection failed : " . mysqli_connect_error());
